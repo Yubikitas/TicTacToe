89a3c204e549b3a6ac0f26468dcaa9443907394f
Pecola <ephialtes11@gmail.com>
2025-02-13 22:21:16 +1100

    game logic operational in console

--- javaScript.js
+++ javaScript.js
@@ -3,13 +3,31 @@ function gameBoard(){
     const columns =3; 
     const board = []; 
 
-    for (let i = 0; i < rows; i++) {
+    for (let i = 0; i < rows; i++) {    
         board[i] = [];
         for (let j = 0; j < columns; j++) {
           board[i].push(Cell());
         }
-      }
+    }
 
+    const getBoard=()=>board; 
+
+    const placeToken = (row,column,player)=>{
+     if (board[row][column].getValue()===0){
+        board[row][column].addToken(player); 
+        console.log('Placed token${player} at ${row},${column}'); 
+     }
+    else{
+        console.log(`Cell at ${row},${column} is already occupied.`); 
+        return; 
+    }
+    }
+
+const printBoard = () =>{ 
+    const boardWithCellValues = board.map((row)=>row.map((cell)=>cell.getValue()))
+    console.log(boardWithCellValues); 
+}
+return {getBoard,placeToken,printBoard}; 
 }
 
 function Cell(){ 
@@ -24,17 +42,54 @@ function Cell(){
         return {
             addToken,
             getValue 
-        }
-    
-}; 
+        };
 
-const placeToken = (location,player)=>{
-    if (location.value==0){
-        board.addToken(player); 
+} 
+
+
+function GameControl(
+    playerOneName = "Player One", 
+    playerTwoName = "Player Two"
+){
+    const board = gameBoard(); 
+
+    const players = [
+        {
+            name: playerOneName, 
+            token: 1
+        },
+        {
+            name: playerTwoName, 
+            token: 2
+        }
+    ]
+
+    let activePlayer = players[0]; 
+
+    const switchPlayerTurn = () =>{
+        activePlayer = activePlayer ===players[0] ? players[1] : players[0]; 
     }
-    else{
-        return; 
+
+    const getActivePlayer = () => activePlayer; 
+
+    const printNewRound =()=>{
+        board.printBoard(); 
+        console.log(`${getActivePlayer().name}'s turn.`); 
+    }; 
+
+    const playRound = (row,column) =>{ 
+        console.log(`Placing ${getActivePlayer().name}'s token into ${row},${column}...`); 
+        board.placeToken(row,column,getActivePlayer().token); 
+        switchPlayerTurn(); 
+        printNewRound(); 
     }
+
+    printNewRound(); 
+
+    return {
+        playRound, 
+        getActivePlayer
+    }; 
 }
 
-
+const game = GameControl(); 
